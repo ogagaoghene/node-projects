1cd6adfd05efa4ab6e50d1b5aac24235c2afd61f
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-06-12 23:38:08 +0100

    Update app.js file

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http = require("http");
+const fs = require('fs');
 const port = process.env.port || 3001
 const app = http.createServer((req, res) => {
    console.log(req.url, req.method);
 
-   //set Header content type 
-   res.setHeader('Content-Type', 'text/plain');
-   res.write('NodeJs is good for backend programming');
-   res.end();
+    fs.readFile('./views/about.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }   
+    });
 });
 
 app.listen(port, 'localhost', () => {
