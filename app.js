794145a64d2b14fcd2957e587cb14a9184c88869
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-06-15 19:28:45 +0100

    First commit for project setup

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.port || 3001;
 
-app.listen(port, function(req, res) {
-    console.log('Server is listening on ' + port);
-});
+app.listen(port);
 
-app.get("/", function (req, res) {
-    res.send('<p>Home Page</p>');
-});
-
-
+app.get('/', function(req, res) {
+    res.send('NodeJs setup');
+})
