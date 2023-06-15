8b8e8007bc589b1bc230e586f63571dcdaa5195d
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-06-15 20:35:12 +0100

    Update app.js, index.html, about.html files to show aa better route handling

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const app = express();
 const port = process.env.port || 3001;
 
 app.get('/', function(req, res) {
-    res.send('<p>Home Page</p>');
+    // res.send('<p>Home Page</p>');
+    res.sendFile('./views/index.html', {root: __dirname});
 });
 
 app.get('/about', function(req, res) {
-    res.send('<p>About Page</p>');
-});
-
-app.get('/service', function(req, res) {
-    res.send('<p>Services Page</p>');
+    // res.send('<p>About Page</p>');
+    res.sendFile('./views/about.html', {root: __dirname});
 });
 
 app.listen(port, function() {
