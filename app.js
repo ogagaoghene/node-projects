7951161e572d82dc8cbabcbdbfc93d72962f848d
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-06-15 20:22:30 +0100

    Update app.js file

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3001;
 
-app.listen(port);
-
 app.get('/', function(req, res) {
-    res.send('NodeJs setup');
-})
+    res.send('<p>Home Page</p>');
+});
+
+app.get('/about', function(req, res) {
+    res.send('<p>About Page</p>');
+});
+
+app.get('/service', function(req, res) {
+    res.send('<p>Services Page</p>');
+});
+
+app.listen(port, function() {
+    console.log(`Server is listening on ${port}`);
+});
+
+
+
